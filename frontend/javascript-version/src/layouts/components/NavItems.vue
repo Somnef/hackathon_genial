284fb6105207dd608ed47e94e7a5fdbf2b121915
qureshiharis <haris.qureshi96@gmail.com>
2024-11-20 15:36:42 +0100

    Added initial frontend files

---
+++ frontend/javascript-version/src/layouts/components/NavItems.vue
@@ -0,0 +1,76 @@
+<script setup>
+import VerticalNavSectionTitle from '@/@layouts/components/VerticalNavSectionTitle.vue';
+import VerticalNavGroup from '@layouts/components/VerticalNavGroup.vue';
+import VerticalNavLink from '@layouts/components/VerticalNavLink.vue';
+</script>
+
+<template>
+  <!-- 👉 Dashboards -->
+  <VerticalNavGroup
+    :item="{
+      title: 'Dashboards',
+      badgeContent: '5',
+      badgeClass: 'bg-error',
+      icon: 'bx-home-smile',
+    }"
+  >
+    <VerticalNavLink
+      :item="{
+        title: 'Analytics',
+        to: '/dashboard',
+      }"
+    />
+  
+  </VerticalNavGroup>
+
+  <!-- 👉 Apps & Pages -->
+  <VerticalNavSectionTitle
+    :item="{
+      heading: 'Apps & Pages',
+    }"
+  />
+
+  <VerticalNavLink
+    :item="{
+      title: 'Account Settings',
+      icon: 'bx-user',
+      to: '/account-settings',
+    }"
+  />
+
+
+  <!-- 👉 User Interface -->
+  <VerticalNavSectionTitle
+    :item="{
+      heading: 'User Interface',
+    }"
+  />
+  <VerticalNavLink
+    :item="{
+      title: 'Cards',
+      icon: 'bx-credit-card',
+      to: '/cards',
+    }"
+  />
+
+  <!-- 👉 Forms & Tables -->
+  <VerticalNavSectionTitle
+    :item="{
+      heading: 'Forms & Tables',
+    }"
+  />
+  <VerticalNavLink
+    :item="{
+      title: 'Form Layouts',
+      icon: 'bx-layout',
+      to: '/form-layouts',
+    }"
+  />
+  <VerticalNavLink
+    :item="{
+      title: 'Tables',
+      icon: 'bx-table',
+      to: '/tables',
+    }"
+  />
+</template>
