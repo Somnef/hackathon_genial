1df4b55b5029ba99b35bc0755cb4964b1481d998
turan.eminli <eminli.turan@gmail.com>
2024-11-20 21:14:35 +0100

    change contract address

--- backend/utils/contractUtil.js
+++ backend/utils/contractUtil.js
@@ -5,7 +5,7 @@ const web3 = new Web3("http://192.168.137.200:7545"); // Replace with your block
 
 // ABI and Contract Address
 const contractABI = require("./contract_abi.json");
-const contractAddress = "0x2d78dfB74d31B65c8E93F3a0CFa3e6D30b627066"; // Replace with your contract's address
+const contractAddress = "0xd50cd37517a073A0c2F8f4A1BA4F930747Ab6cCB"; // Replace with your contract's address
 
 // Utility function to get the contract instance
 const getContractInstance = () => {
