a38b9d3caa46c61ca7b6caa038ec411b1d60f398
qureshiharis <haris.qureshi96@gmail.com>
2024-11-21 13:01:27 +0100

    Updating bidding and landing page

--- frontend/javascript-version/src/views/pages/cards/card-basic/CardBasic.vue
+++ frontend/javascript-version/src/views/pages/cards/card-basic/CardBasic.vue
@@ -4,6 +4,7 @@ import avatar2 from '@images/avatars/avatar-2.png'
 import avatar3 from '@images/avatars/avatar-3.png'
 import avatar4 from '@images/avatars/avatar-4.png'
 import eCommerce2 from '@images/eCommerce/2.png'
+import { ref } from 'vue'
 
 const avatars = [
   avatar1,
@@ -13,283 +14,124 @@ const avatars = [
 ]
 
 const isCardDetailsVisible = ref(false)
+
+// Alert state
+const alertVisible = ref(false)
+const alertMessage = ref('')
+
+// Handle Buy Button Click
+function handleBuyClick() {
+  alertMessage.value = 'You have purchased successfully!'
+  alertVisible.value = true
+
+  // Auto-dismiss the alert after 3 seconds
+  setTimeout(() => {
+    alertVisible.value = false
+  }, 3000)
+}
 </script>
 
 <template>
-  <VRow>
-
-
-    <VCol
-      sm="6"
-      cols="12"
+  <VContainer>
+    <!-- Success Alert -->
+    <VAlert
+      v-if="alertVisible"
+      type="success"
+      class="mb-4"
+      dismissible
+      @click:close="alertVisible = false"
     >
-      <VCard>
-        <div class="d-flex justify-space-between flex-wrap flex-md-nowrap flex-column flex-md-row">
-          <div class="ma-auto pa-5">
-            <VImg
-              width="137"
-              height="176"
-              :src="eCommerce2"
-            />
-          </div>
+      {{ alertMessage }}
+    </VAlert>
 
-          <VDivider :vertical="$vuetify.display.mdAndUp" />
-
-          <div>
-            <VCardItem>
-              <VCardTitle>Energy Units 15000 Kilo Watts</VCardTitle>
-            </VCardItem>
-
-            <VCardText>
-              Description for the text
-            </VCardText>
-
-            <VCardText class="text-subtitle-1">
-              <span>Price :</span> <span class="font-weight-medium">$899</span>
-            </VCardText>
-
-            <VCardActions class="justify-space-between">
-              <VBtn>
-                <VIcon icon="bx-cart-add" />
-                <span class="ms-2">Buy</span>
-              </VBtn>
-
-              <VBtn
-                color="secondary"
-                icon="bx-share-alt"
+    <VRow>
+      <VCol
+        sm="6"
+        cols="12"
+      >
+        <VCard>
+          <div class="d-flex justify-space-between flex-wrap flex-md-nowrap flex-column flex-md-row">
+            <div class="ma-auto pa-5">
+              <VImg
+                width="137"
+                height="176"
+                :src="eCommerce2"
               />
-            </VCardActions>
+            </div>
+
+            <VDivider :vertical="$vuetify.display.mdAndUp" />
+
+            <div>
+              <VCardItem>
+                <VCardTitle>Energy Units 15000 Kilo Watts</VCardTitle>
+              </VCardItem>
+
+              <VCardText>
+                Description for the text
+              </VCardText>
+
+              <VCardText class="text-subtitle-1">
+                <span>Price :</span> <span class="font-weight-medium">$899</span>
+              </VCardText>
+
+              <VCardActions class="justify-space-between">
+                <VBtn @click="handleBuyClick">
+                  <VIcon icon="bx-cart-add" />
+                  <span class="ms-2">Buy</span>
+                </VBtn>
+              </VCardActions>
+            </div>
           </div>
-        </div>
-      </VCard>
-    </VCol>
+        </VCard>
+      </VCol>
 
-
-    <!-- Second card item -->
-    <VCol
-      sm="6"
-      cols="12"
-    >
-      <VCard>
-        <div class="d-flex justify-space-between flex-wrap flex-md-nowrap flex-column flex-md-row">
-          <div class="ma-auto pa-5">
-            <VImg
-              width="137"
-              height="176"
-              :src="eCommerce2"
-            />
-          </div>
-
-          <VDivider :vertical="$vuetify.display.mdAndUp" />
-
-          <div>
-            <VCardItem>
-              <VCardTitle>Energy Units 15000 Kilo Watts</VCardTitle>
-            </VCardItem>
-
-            <VCardText>
-              Description for the text
-            </VCardText>
-
-            <VCardText class="text-subtitle-1">
-              <span>Price :</span> <span class="font-weight-medium">$899</span>
-            </VCardText>
-
-            <VCardActions class="justify-space-between">
-              <VBtn>
-                <VIcon icon="bx-cart-add" />
-                <span class="ms-2">Buy</span>
-              </VBtn>
-
-              <VBtn
-                color="secondary"
-                icon="bx-share-alt"
+      <!-- Additional Cards (unchanged) -->
+      <VCol
+        sm="6"
+        cols="12"
+      >
+        <VCard>
+          <div class="d-flex justify-space-between flex-wrap flex-md-nowrap flex-column flex-md-row">
+            <div class="ma-auto pa-5">
+              <VImg
+                width="137"
+                height="176"
+                :src="eCommerce2"
               />
-            </VCardActions>
+            </div>
+
+            <VDivider :vertical="$vuetify.display.mdAndUp" />
+
+            <div>
+              <VCardItem>
+                <VCardTitle>Energy Units 15000 Kilo Watts</VCardTitle>
+              </VCardItem>
+
+              <VCardText>
+                Description for the text
+              </VCardText>
+
+              <VCardText class="text-subtitle-1">
+                <span>Price :</span> <span class="font-weight-medium">$899</span>
+              </VCardText>
+
+              <VCardActions class="justify-space-between">
+                <VBtn @click="handleBuyClick">
+                  <VIcon icon="bx-cart-add" />
+                  <span class="ms-2">Buy</span>
+                </VBtn>
+
+                <VBtn
+                  color="secondary"
+                  icon="bx-share-alt"
+                />
+              </VCardActions>
+            </div>
           </div>
-        </div>
-      </VCard>
-    </VCol>
-
-    <VCol
-      sm="6"
-      cols="12"
-    >
-      <VCard>
-        <div class="d-flex justify-space-between flex-wrap flex-md-nowrap flex-column flex-md-row">
-          <div class="ma-auto pa-5">
-            <VImg
-              width="137"
-              height="176"
-              :src="eCommerce2"
-            />
-          </div>
-
-          <VDivider :vertical="$vuetify.display.mdAndUp" />
-
-          <div>
-            <VCardItem>
-              <VCardTitle>Energy Units 15000 Kilo Watts</VCardTitle>
-            </VCardItem>
-
-            <VCardText>
-              Description for the text
-            </VCardText>
-
-            <VCardText class="text-subtitle-1">
-              <span>Price :</span> <span class="font-weight-medium">$899</span>
-            </VCardText>
-
-            <VCardActions class="justify-space-between">
-              <VBtn>
-                <VIcon icon="bx-cart-add" />
-                <span class="ms-2">Buy</span>
-              </VBtn>
-
-              <VBtn
-                color="secondary"
-                icon="bx-share-alt"
-              />
-            </VCardActions>
-          </div>
-        </div>
-      </VCard>
-    </VCol>
-
-    <VCol
-      sm="6"
-      cols="12"
-    >
-      <VCard>
-        <div class="d-flex justify-space-between flex-wrap flex-md-nowrap flex-column flex-md-row">
-          <div class="ma-auto pa-5">
-            <VImg
-              width="137"
-              height="176"
-              :src="eCommerce2"
-            />
-          </div>
-
-          <VDivider :vertical="$vuetify.display.mdAndUp" />
-
-          <div>
-            <VCardItem>
-              <VCardTitle>Energy Units 15000 Kilo Watts</VCardTitle>
-            </VCardItem>
-
-            <VCardText>
-              Description for the text
-            </VCardText>
-
-            <VCardText class="text-subtitle-1">
-              <span>Price :</span> <span class="font-weight-medium">$899</span>
-            </VCardText>
-
-            <VCardActions class="justify-space-between">
-              <VBtn>
-                <VIcon icon="bx-cart-add" />
-                <span class="ms-2">Buy</span>
-              </VBtn>
-
-              <VBtn
-                color="secondary"
-                icon="bx-share-alt"
-              />
-            </VCardActions>
-          </div>
-        </div>
-      </VCard>
-    </VCol>
-
-    <VCol
-      sm="6"
-      cols="12"
-    >
-      <VCard>
-        <div class="d-flex justify-space-between flex-wrap flex-md-nowrap flex-column flex-md-row">
-          <div class="ma-auto pa-5">
-            <VImg
-              width="137"
-              height="176"
-              :src="eCommerce2"
-            />
-          </div>
-
-          <VDivider :vertical="$vuetify.display.mdAndUp" />
-
-          <div>
-            <VCardItem>
-              <VCardTitle>Energy Units 15000 Kilo Watts</VCardTitle>
-            </VCardItem>
-
-            <VCardText>
-              Description for the text
-            </VCardText>
-
-            <VCardText class="text-subtitle-1">
-              <span>Price :</span> <span class="font-weight-medium">$899</span>
-            </VCardText>
-
-            <VCardActions class="justify-space-between">
-              <VBtn>
-                <VIcon icon="bx-cart-add" />
-                <span class="ms-2">Buy</span>
-              </VBtn>
-
-              <VBtn
-                color="secondary"
-                icon="bx-share-alt"
-              />
-            </VCardActions>
-          </div>
-        </div>
-      </VCard>
-    </VCol>
-    <VCol
-      sm="6"
-      cols="12"
-    >
-      <VCard>
-        <div class="d-flex justify-space-between flex-wrap flex-md-nowrap flex-column flex-md-row">
-          <div class="ma-auto pa-5">
-            <VImg
-              width="137"
-              height="176"
-              :src="eCommerce2"
-            />
-          </div>
-
-          <VDivider :vertical="$vuetify.display.mdAndUp" />
-
-          <div>
-            <VCardItem>
-              <VCardTitle>Energy Units 15000 Kilo Watts</VCardTitle>
-            </VCardItem>
-
-            <VCardText>
-              Description for the text
-            </VCardText>
-
-            <VCardText class="text-subtitle-1">
-              <span>Price :</span> <span class="font-weight-medium">$899</span>
-            </VCardText>
-
-            <VCardActions class="justify-space-between">
-              <VBtn>
-                <VIcon icon="bx-cart-add" />
-                <span class="ms-2">Buy</span>
-              </VBtn>
-
-              <VBtn
-                color="secondary"
-                icon="bx-share-alt"
-              />
-            </VCardActions>
-          </div>
-        </div>
-      </VCard>
-    </VCol>
-  </VRow>
+        </VCard>
+      </VCol>
+    </VRow>
+  </VContainer>
 </template>
 
 <style lang="scss" scoped>
