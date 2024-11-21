8afa7a282f6ba28e19bb9f66bd38dbd9f46296f9
turan.eminli <eminli.turan@gmail.com>
2024-11-21 09:41:39 +0100

    Migrate the contract creds to files

--- backend/utils/contractUtil.js
+++ backend/utils/contractUtil.js
@@ -1,13 +1,10 @@
 const { Web3 } = require("web3");
 
-// Initialize Web3
-const web3 = new Web3("http://192.168.137.200:7545"); // Replace with your blockchain node URL
+const web3 = new Web3("http://192.168.137.200:7545");
 
-// ABI and Contract Address
 const contractABI = require("./contract_abi.json");
-const contractAddress = "0xd50cd37517a073A0c2F8f4A1BA4F930747Ab6cCB"; // Replace with your contract's address
+const contractAddress = require("./contract_address.json").contractAddress;
 
-// Utility function to get the contract instance
 const getContractInstance = () => {
   return new web3.eth.Contract(contractABI, contractAddress);
 };
